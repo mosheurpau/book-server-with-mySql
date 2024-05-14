ef40cc37da43c7521def7004569146490c442645
mosheurpau <mosheur.pau@gamil.com>
2024-05-14 09:57:20 +0600

    books backend

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "admin",
+  database: "test",
+});
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM test.books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  // Check if required data exists
+  if (!req.body.title || !req.body.desc || !req.body.cover) {
+    return res
+      .status(400)
+      .json({ message: "Missing required fields in request body" });
+  }
+
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend!");
+});
